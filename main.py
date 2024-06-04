8775180a0df315ce9f5398e4af0b8592f055d5e7
neko ( <neko (@HomeBunsen)>
2024-06-05 00:12:39 +0700

    Add default handler (no commands) to the bot

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import telebot
 import logging
 
 import wordbank
+
 # logging.basicConfig(level=logging.DEBUG)
 # logging.basicConfig(level=logging.INFO)
 logging.basicConfig(
@@ -40,6 +41,7 @@ def start(message):
 def id_(message):
     bot.send_message(message.chat.id, f'Your id is {message.from_user.id}', parse_mode='html')
 
+
 def get_text(message):
     if len(message.text.split(' ')) < 2:
         # if message.reply_to_message and message.reply_to_message.text:
@@ -51,9 +53,9 @@ def get_text(message):
     else:
         return message.text.split(' ')[1]
 
+
 @bot.message_handler(commands=['s', 'suggest'])
 def suggest(message):
-
     num = get_text(message)
     # check
     # if num and all(num, lambda x: x.isdigit()):
@@ -63,12 +65,31 @@ def suggest(message):
         bot.send_message(message.chat.id, f'Words for {num}: {words}', parse_mode='html')
     else:
         bot.send_message(message.chat.id, f'Expected digits only, but got "{num}"', parse_mode='html')
+
+
 # if len(message.text.split(' ')) < 2:
 
 # from mailbox import MailBox
 # import mailbox
 # from status import Status
 # from mailbox import MailBox
+
+# @bot.callback_query_handler(func=lambda call: True)
+@bot.message_handler(func=lambda message: True)
+def callback_all(message):
+    num = message.text
+    if len(parts := message.text.split(' ')) < 2:
+        # return ''
+        pass
+    else:
+        num = parts[1]
+    if num.isdigit():
+        words = wb.suggest_words_for_num(num)
+        bot.send_message(message.chat.id, f'Words for {num}: {words}', parse_mode='html')
+    else:
+        # Translate word to number
+        new_num = wb.word_to_number(num)
+        bot.send_message(message.chat.id, f'Number for "{num}": "{new_num}"', parse_mode='html')
 
 
 def pair_of_ints(s):
@@ -78,15 +99,14 @@ def pair_of_ints(s):
     return pair
 
 
-
-
-
 def main():
     bot.polling(none_stop=True)
 
 
 if __name__ == '__main__':
     import wordbank
+
     global wb
-    wb = wordbank.WordBank(wordfile=wordbank.WORD_RUS_TXT)
+    # wb = wordbank.WordBank(wordfile=wordbank.WORD_RUS_TXT)
+    wb = wordbank.WordBank(wordfile=wordbank.ZDF_UTF_TXT)
     main()
