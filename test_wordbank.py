c37809c86838ad30e87dbe441f53b40ce5f64763
neko ( <neko (@HomeBunsen)>
2024-06-04 21:50:24 +0700

    Add load_words_from_file() with tests

--- test_wordbank.py
+++ test_wordbank.py
@@ -9,8 +9,15 @@ class MyTestCase(unittest.TestCase):
 
     def test_load_words_rus(self):
         wb = WordBank()
-        self.assertIsNotNone(wb)  # add assertion here
+        loaded = wb.load_words_from_file(filename='./data/word_rus.txt')
+        # self.assertEqual(loaded, 34009)  # wc -l
+        self.assertEqual(loaded, 34010)  # fix for no eol
 
+    def test_load_zdf(self):
+        wb = WordBank()
+        loaded = wb.load_words_from_file(filename='./data/zdf-utf8.txt')
+        # self.assertEqual(loaded, 93391)  # wc -l
+        self.assertEqual(loaded, 93372)  # fix for no eol
 
 if __name__ == '__main__':
     unittest.main()
--- wordbank.py
+++ wordbank.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class WordBank:
     num_to_chars = ['нл', 'кг', 'бхц', 'тз', 'чр', 'п', 'шщж', 'см', 'вф', 'д']
 
@@ -10,14 +13,48 @@ class WordBank:
             char_to_num[ch] = num
 
     def __init__(self, config=None, wordfile=None):
-        self.config = config if config is not None else {}
+        self.config = config if config is not None else defaultdict(str)
+        self.num_to_words = defaultdict(list)
         if wordfile is not None:
             self.wordfile = wordfile
-            config['wordfile'] = wordfile
+            self.config['wordfile'] = wordfile
         else:
-            self.wordfile = config['wordfile']
+            # self.wordfile = self.config['wordfile']
+            ...
 
-    def load_words_from_file(self):
+    # @staticmethod
+    def word_to_number(self, word: str) -> str:
+        # logging.debug('number(%s)', w)
+        # print(w)
+        num_str = ''
+        for ch in word:
+            if ch in __class__.char_to_num:
+                num_str += str(__class__.char_to_num[ch])
+
+        # logging.debug('number(%s)=%s', w, num_str)
+        if num_str != '':
+            return num_str  # As string !! Keep the leading zeroes!!
+        return None
+
+    def load_words_from_file(self, filename: str = None) -> int:
         # with io.open(WORKFILE, 'r', encoding="utf-8") as f:
-        with open(self.wordfile, 'r', encoding="utf-8") as f:
-            read_data = f.readlines()
+        if filename is None:
+            filename = self.wordfile
+        with open(filename, 'r', encoding="utf-8") as f:
+            # read_data = f.readlines()
+            found_count = 0
+            for word in f.readlines():
+                word = word.strip()
+                num = self.word_to_number(word)
+                # num = __class__.word_to_number(word)
+
+                # logging.debug(word, num)
+                # logging.debug(word)
+                if num is None:
+                    # print(f'FAIL for "{word}"')
+                    continue  # No consonants
+                # logging.debug(word, num)
+                self.num_to_words[num].append(word)
+                found_count += 1
+            print(word)
+        return found_count
